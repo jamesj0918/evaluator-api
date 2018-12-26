07f59c96515cc68a75cf5d3ffad1d313de04c05b
James <jamesj0107@naver.com>
2018-12-27 02:19:58 +0900

    Fix Constant names to follow PEP8 style guidelines and add Lecture Model

--- apps/evaluator/models.py
+++ apps/evaluator/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Department(models.Model):
-    Department_Choices = (
+    DEPARTMENT_CHOICES = (
         ("컴퓨터공학과", "Computer Engineering"),
         ("소프트웨어과", "Software"),
         ("정보보호학과", "Information Security"),
@@ -10,34 +10,33 @@ class Department(models.Model):
         ("데이터사이언스", "Data Science"),
     )
 
-    title = models.CharField(choices=Department_Choices, max_length=32)
+    title = models.CharField(choices=DEPARTMENT_CHOICES, max_length=32)
 
     def __str__(self):
         return self.title
 
 
-class Question(models.Model):
-    question_num = models.CharField(max_length=8)
-    points = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.question_num
-
-
 class Test(models.Model):
-    Lecture_Choices = (
+    LECTURE_CHOICES = (
         (0, "C프로그래밍및실습"),
         (1, "고급C프로그래밍및실습")
     )
 
     year = models.IntegerField(default=2018)
     semester = models.IntegerField(default=1)
-    lecture_type = models.IntegerField(choices=Lecture_Choices)
-
-    questions = models.ManyToManyField(Question)
+    lecture_type = models.IntegerField(choices=LECTURE_CHOICES)
 
     def __str__(self):
         return str(self.year) + "-" + str(self.semester)
+
+
+class Question(models.Model):
+    question_num = models.CharField(max_length=8)
+    points = models.IntegerField(default=0)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name="question", related_query_name="question")
+
+    def __str__(self):
+        return self.question_num
 
 
 class Student(models.Model):
@@ -45,8 +44,15 @@ class Student(models.Model):
     name = models.CharField(max_length=16)
     grade = models.IntegerField(default=1)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    test = models.ManyToManyField(Test, blank=True, null=True)
 
     def __str__(self):
         return self.student_id
 
+
+class Lecture(models.Model):
+    code = models.CharField(max_length=32, unique=True)
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="lecture", related_query_name="lecture")
+
+    def __str__(self):
+        return self.code
